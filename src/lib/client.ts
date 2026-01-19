47a729a172dc6e1dc2dac72f2ad7eba712f8c1d4
Xlussov <130926999+Xlussov@users.noreply.github.com>
2026-01-19 09:38:35 +0200

    fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,4 @@
 import { treaty } from '@elysiajs/eden';
 import type { App } from '../app/api/[[...slugs]]/route';
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
-export const client = treaty<App>(baseUrl).api;
+export const client = treaty<App>(process.env.VERCEL_URL ?? 'localhost:3000').api;
